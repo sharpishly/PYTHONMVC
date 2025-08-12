f1263d6ea179bc3901c43cab4f7ded97d7a8de50
Steve Austin <steve@austin.com>
2025-08-12 15:51:38 +0100

    Added git ignore file and removed shell scripts

--- build.sh
+++
@@ -1,6 +0,0 @@
-# Build image
-docker build -t python-mvc .
-
-# Run container in background, map port 3000
-# Remember to update Dockerfile if you change ports
-docker run -d --name mvc-app -p 3000:3000 python-mvc
